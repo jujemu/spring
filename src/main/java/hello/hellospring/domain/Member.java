12928a815085d701848c6036552f35f22d12f03f
jujemu <jujemu30@gmail.com>
2023-07-03 20:09:01 +0900

    Feat: member 도메인, 레포지토리 생성
    - Member 클래스 생성
    - MemberRepository interface와 memory 구현체 생성

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,19 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
